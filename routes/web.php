690695a29609bab29ce72ab08b87b779a397b8ae
Huzaifa <huzaifa.adil@devparagon.com>
2026-08-13 05:51:26 +0500

    settings route fixed

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::middleware(['auth', 'verified', 'current.organization'])->group(function 
     Route::get('dashboard', function () {
         return redirect()->to(Auth::user()?->getDashboardUrl() ?? route('home'));
     })->name('dashboard');
+
+    require __DIR__.'/settings.php';
 });
 
-require __DIR__.'/settings.php';
